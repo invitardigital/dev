bec3918ca1b4626a85c7da46da011695336cc518
gus <griselamore552020@gmail.com>
2026-05-31 00:53:55 -0600

    S

--- views/partials/navbar.blade.php
+++ views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">MiApp TEST</a>
+        <a class="navbar-brand" href="{{ url('/') }}">MiApp TEST 2</a>
         <div class="navbar-nav ms-auto">
             @if(auth()->check())
                 <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
