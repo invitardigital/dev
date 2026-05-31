0310fce2ec0ba77b96a4927d36748f1c4901cd70
gus <griselamore552020@gmail.com>
2026-05-31 01:06:11 -0600

    S

--- views/partials/navbar.blade.php
+++ views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">MiAAA</a>
+        <a class="navbar-brand" href="{{ url('/') }}">MiSSS</a>
         <div class="navbar-nav ms-auto">
             @if(auth()->check())
                 <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
